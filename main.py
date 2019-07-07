38e2f1ef40feb8b3861f527e6a8185e812ff9c6d
Tom Garwood <4235553+tomgarwood@users.noreply.github.com>
2019-07-07 15:02:04 +1000

    Flask webapp initial commit

--- main.py
+++ main.py
@@ -5,24 +5,27 @@ from joblib import dump, load
 from user_input import getpqe, getlocation, gettier
 
 # import dataset into a pandas dataframe
+"""
 data = pd.read_csv('data/pqe_data_tier_city.csv')
 data['tier'] = data['tier'].replace({1: 'top', 2: 'mid', 3: 'small'})
 data = pd.get_dummies(data, columns=['tier'])
 data['location'] = data['location'].replace({1: 'syd', 2: 'melb', 3: 'bris', 4: 'per'})
 data = pd.get_dummies(data, columns=['location'])
+"""
 
 # create test/train data split
+"""
 X = data.drop('package', axis=1)
 y = data[['package']]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/3, random_state=0)
+"""
 
 # train machine learning regression model
+"""
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
-dump(regressor, 'model.joblib')  # dump trained model for future use
-
-# uncomment to load trained model
-# regressor = load('model.joblib')
+dump(regressor, 'model/model.joblib')  # dump trained model for future use
+"""
 
 # test tools, uncomment to review
 """
@@ -31,22 +34,22 @@ import math
 for i, col_name in enumerate(X_train.columns):
     print("The coefficient for {} is {}".format(col_name, regressor.coef_[0][i]))
 intercept = regressor.intercept_[0]
-print("The intercept for this model is {}".format(intercept))
+print("The model incercept is {}".format(intercept))
 y_predit = regressor.predict(X_test)
 regressor_mse = mean_squared_error(y_predit, y_test)
 print(math.sqrt(regressor_mse))
 print(regressor.score(X_test, y_test))
 """
-pqe = getpqe()
-loc = getlocation()
-syd = float(1) if loc == 'syd' else float(0)
-mel = float(1) if loc == 'mel' else float(0)
-bris = float(1) if loc == 'bris' else float(0)
-per = float(1) if loc == 'per' else float(0)
-tier = gettier()
-top = float(1) if tier == 'top' else float(0)
-mid = float(1) if tier == 'mid' else float(0)
-small = float(1) if tier == 'small' else float(0)
 
-print(regressor.predict([[pqe, mid, small, top, bris, mel, per, syd]]))
-
+# prediction output from regressor model
+def regressor(loc, pqe, tier):
+    syd = float(1) if loc == 'syd' else float(0)
+    mel = float(1) if loc == 'mel' else float(0)
+    bris = float(1) if loc == 'bris' else float(0)
+    per = float(1) if loc == 'per' else float(0)
+    top = float(1) if tier == 'top' else float(0)
+    mid = float(1) if tier == 'mid' else float(0)
+    small = float(1) if tier == 'small' else float(0)
+    regressor = load('model/model.joblib')  # loading pre-trained model
+    result = regressor.predict([[pqe, mid, small, top, bris, mel, per, syd]])
+    return result[0][0]
---
+++ webapp.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+from main import regressor
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def userform():
+    loc = request.form['loc']
+    pqe = float(request.form['pqe'])
+    tier = request.form['tier']
+    salary = '${:0,.0f}'.format(regressor(loc, pqe, tier))
+    return render_template('result.html', salary=salary, loc=loc, pqe=pqe, tier=tier)
+
+if __name__ == "__main__":
+    app.run()
